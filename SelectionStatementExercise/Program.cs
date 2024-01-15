be199ea60676c25477ac06809c0427409af751e8
will street <will@williamdstreet.com>
2024-01-15 14:49:25 -0600

    first commit, if else, working

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Try and guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+
+            if (favNumber > userInput)
+            {
+                Console.WriteLine("Too Low");
+
+            }
+           
+            
+            else if (favNumber < userInput)
+            {
+                Console.WriteLine("Too high");
+
+            }
+
+
+            else 
+            {
+
+                Console.WriteLine("Correct!");
+            }
         }
     }
 }
